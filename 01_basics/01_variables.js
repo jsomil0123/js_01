fb9cb4eb5040e49f28f6f36e6b25920239338787
jsomil0123 <94337948+jsomil0123@users.noreply.github.com>
2024-09-11 03:30:52 +0000

    do the necessary

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountID = 144553
+let accountEmail = "somil@weknowtech.in"
+var accountPassword = "12345"
+accountCity = "Varanasi"
+let accountState;
+
+// accountID = 2        not alloallowed 
+accountEmail = "somil@gmail.com"
+accountPassword = "13579" 
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
+
+/* Prefer not to use VAR, because in issues in block scope & functional scope  */
