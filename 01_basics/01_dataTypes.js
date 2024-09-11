211184f364927b41b2e08af9a576c63c2ce899f7
jsomil0123 <94337948+jsomil0123@users.noreply.github.com>
2024-09-11 03:52:27 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict";  // treat all JS code as newer version
+// alert(3+3)  // we are using node not browser
+console.log(3 + 3);
+
+// tc39.es website name
+
+console.log(typeof "Somil");
+console.log(typeof null);       // object 
+console.log(typeof undefined);  // undefined
